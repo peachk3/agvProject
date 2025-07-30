d72c869b772fe4e32784c3ded6af9498e97f2a93
Yonadada <yknda123@gmail.com>
2025-07-30 16:59:51 +0900

    기초설정(2)

--- agvProject/App.xaml.cs
+++ agvProject/App.xaml.cs
@@ -9,6 +9,10 @@ namespace agvProject
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
     }
 
 }
