34af05793a3f63eaf8ce2b871c89c9b8db38a175
suuurin <z11673@naver.com>
2025-08-05 14:10:53 +0900

    feat: 메인 모니터링 화면(MainView) 정렬 개선 및 미션 등록 이벤트 추가

--- agvProject/Views/NewMissionDialog.xaml.cs
+++ agvProject/Views/NewMissionDialog.xaml.cs
@@ -16,21 +16,13 @@ using MahApps.Metro.Controls;
 namespace agvProject.Views
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// NewMissionDialog.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class NewMissionDialog : MetroWindow
     {
-        public MainWindow()
+        public NewMissionDialog()
         {
             InitializeComponent();
-        }
-
-        private void NewMission_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new NewMissionDialog();
-            dialog.Owner = this;    // 메인창 위에 뜨게
-            bool? result = dialog.ShowDialog();
-            
         }
     }
 }
